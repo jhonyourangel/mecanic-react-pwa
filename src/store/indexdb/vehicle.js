ab52bc808c6071375213a209d8be187686e7daf9
ion utale <ion.utale@icloud.com>
2018-09-06 18:31:34 +0200

    commit vehicle idb file

---
+++ src/store/indexdb/vehicle.js
@@ -0,0 +1,30 @@
+import idb from 'idb';
+
+
+const dbPromise = idb.open('mecanic', 1, upgradeDB => {
+    let vehicleStore = upgradeDB.createObjectStore('vehicle', {
+        keyPath: 'plateNumber'
+    })
+    vehicleStore.createIndex('syncStatus', 'syncStatus')
+})
+
+// this does an upsert 
+// so no need to create an edit vehicle method
+export const createVehicle = (vehicle) => {
+    return dbPromise.then(db => {
+        const tx = db.transaction('vehicle', 'readwrite')
+
+        tx.objectStore('vehicle').put({ ...vehicle,
+            syncStatus: vehicle.syncStatus || 'synced'
+        });
+        return tx.complete;
+    })
+}
+
+export const getAllVehicle = (filterValue) => {
+    return dbPromise.then(db => {
+        const syncStatusIndex = db.transaction('vehicle', 'readwrite')
+            .objectStore('vehicle').index('syncStatus')
+        return syncStatusIndex.getAll(filterValue === '' ? undefined : filterValue )
+    })
+}
