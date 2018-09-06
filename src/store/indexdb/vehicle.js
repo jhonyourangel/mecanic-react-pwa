358fdf414cb8838dd14a7ffb988479198534d05d
ion utale <ion.utale@icloud.com>
2018-09-06 18:32:43 +0200

    fixed

--- src/store/indexdb/vehicle.js
+++ src/store/indexdb/vehicle.js
@@ -10,9 +10,19 @@ const dbPromise = idb.open('mecanic', 1, upgradeDB => {
 
 // this does an upsert 
 // so no need to create an edit vehicle method
-export const createVehicle = (vehicle) => {
+export const createVehicle = (vehicle, index) => {
     return dbPromise.then(db => {
-        const tx = db.transaction('vehicle', 'readwrite')
+        const tx = db.transaction('vehicle', 'readwrite');
+
+        if (index < 5) {
+            vehicle.syncStatus = 'new'
+        } else if (index < 10) {
+            vehicle.syncStatus = 'edit'
+        } else if (index < 15) {
+            vehicle.syncStatus = 'delete'
+        } else {
+            vehicle.syncStatus = undefined
+        }
 
         tx.objectStore('vehicle').put({ ...vehicle,
             syncStatus: vehicle.syncStatus || 'synced'
@@ -25,6 +35,6 @@ export const getAllVehicle = (filterValue) => {
     return dbPromise.then(db => {
         const syncStatusIndex = db.transaction('vehicle', 'readwrite')
             .objectStore('vehicle').index('syncStatus')
-        return syncStatusIndex.getAll(filterValue === '' ? undefined : filterValue )
-    })
+            return syncStatusIndex.getAll(filterValue === '' ? undefined : filterValue )
+        })
 }
