25de5c8474b7f26cb76830bf549520df5efa178f
ion utale <ion.utale@icloud.com>
2018-09-24 08:52:10 +0200

    what is that util ? REMOVEEEE

--- src/navigation/SideDrawer/SideDrawer.test.js
+++ src/navigation/SideDrawer/SideDrawer.test.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { shallow, configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 import SideDrawer from './SideDrawer'
-import { log } from 'util';
 
 configure({adapter: new Adapter()})
 
