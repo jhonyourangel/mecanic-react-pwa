53e0e89c4b63c8a5677c72a9090a784765b6bca6
ion utale <ion.utale@icloud.com>
2018-10-01 13:09:56 +0200

    created container intretineri

---
+++ src/containers/intretineri/intretineri.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import css from './intretineri.module.css'
+
+class Intretineri extends Component {
+    render() {
+        return (
+            <div>Intretineri</div>
+        )
+    }
+}
+
+export default Intretineri
