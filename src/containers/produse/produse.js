50e00ef7f933f2170de447c6a7c418a339398462
ion utale <ion.utale@icloud.com>
2018-10-01 13:10:12 +0200

    created container products

---
+++ src/containers/produse/produse.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import css from './produse.module.css'
+
+class Produse extends Component {
+    render() {
+        return (
+            <div>Produse</div>
+        )
+    }
+}
+
+export default Produse
