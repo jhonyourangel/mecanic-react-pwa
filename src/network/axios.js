8b76491f68e7543fc0ff6f4f0069c380b34ede00
ion utale <ion.utale@icloud.com>
2018-10-15 19:21:37 +0200

    move link to prod server, for demo purpose

--- src/network/axios.js
+++ src/network/axios.js
@@ -4,7 +4,7 @@ const PROD_URL = 'https://mecanic.herokuapp.com/api/pwa/'
 const DEV_URL = 'http://localhost:8080/api/pwa/'
 
 const instance = axios.create({
-    baseURL: DEV_URL,
+    baseURL: PROD_URL,
     headers: {
         common: { 'Authorization': 'Bearer ' + localStorage.getItem('token')}
     } 
