adf1d2042c430283a70187b2b10c75197f5c4332
ion utale <ion.utale@icloud.com>
2018-10-24 18:08:40 +0200

    maintenance cell

---
+++ src/containers/maintenance/maintenanceCell/maintenanceCell.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import css from './maintenanceCell.module.css'
+
+import moment from 'moment'
+
+class MaintenanceCell extends Component {
+    render() {
+        const {
+            creationDate,
+            editDate,
+            createdBy,
+            products,
+            category,
+            plateNumber,
+            vin,
+            carKm,
+            note,
+            _id
+            } = this.props.maintenance;
+        
+        return (
+            <article className={css.MaintenanceCell}>
+                <Link to={`/intretinere/${_id}`}>
+                    <section>
+                        <h3>{plateNumber}</h3>
+                        <p className={css.CreationDate}>{moment(creationDate).format('DD MMM YYYY')}</p>
+                    </section>
+                    <p>km: {carKm}</p>
+                </Link>
+            </article>
+        )
+    }
+}
+
+export default MaintenanceCell
