21f8db0d5459aff64b8b05513bcd169c363aa2e5
ion utale <ion.utale@icloud.com>
2018-10-24 18:08:54 +0200

    maintenance page

---
+++ src/containers/maintenance/maintenancePage/maintenancePage.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import css from './maintenancePage.module.css'
+
+class MaintenancePage extends Component {
+    render() {
+        return (
+            <div>MaintenancePage</div>
+        )
+    }
+}
+
+export default MaintenancePage
